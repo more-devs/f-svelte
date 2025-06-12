ff11db8cc679d6f2057cd517f115d86f0887ac94
more-devs <moresitoes@gmail.com>
2025-06-12 08:17:59 -0400

    1ercomit

---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { onMount } from 'svelte';
+	import { obtenerPersonas, crearPersona, actualizarPersona, eliminarPersona } from './api.js';
+
+	let personas = [];
+	let nombre = '';
+	let edad = '';
+	let editando = null;
+
+	onMount(async () => {
+		personas = await obtenerPersonas();
+	});
+
+	async function guardar() {
+		if (editando) {
+			await actualizarPersona(editando.id, { nombre, edad });
+			editando = null;
+		} else {
+			await crearPersona({ nombre, edad });
+		}
+		nombre = '';
+		edad = '';
+		personas = await obtenerPersonas();
+	}
+
+	function editar(persona) {
+		nombre = persona.nombre;
+		edad = persona.edad;
+		editando = persona;
+	}
+
+	async function eliminar(id) {
+		await eliminarPersona(id);
+		personas = await obtenerPersonas();
+	}
+</script>
+
+<h1>Personas</h1>
+
+<form on:submit|preventDefault={guardar}>
+	<input bind:value={nombre} placeholder="Nombre" required />
+	<input type="number" bind:value={edad} placeholder="Edad" required />
+	<button type="submit">{editando ? 'Actualizar' : 'Agregar'}</button>
+</form>
+
+<ul>
+	{#each personas as persona}
+		<li>
+			{persona.nombre} ({persona.edad} años)
+			<button on:click={() => editar(persona)}>Editar</button>
+			<button on:click={() => eliminar(persona.id)}>Eliminar</button>
+		</li>
+	{/each}
+</ul>
